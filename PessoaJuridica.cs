1c0400ff189802fc74980264226ac105fb96b52f
Gabriela Diniz <gadisa10@outlook.com>
2023-01-12 12:49:47 -0300

    definindo classes

---
+++ PessoaJuridica.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class PessoaJuridica
+    {
+        string cnpj;
+        string ie;
+
+        void pagar_Imposto(float valor) {
+            
+        }
+    }
+}
